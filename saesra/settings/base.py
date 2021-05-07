a86495d8a97ce312a63056794ee0e28c8e87b6d7
Akın Oslu <aknoslu3541@gmail.com>
2021-05-08 02:45:45 +0300

    media dosyaları sunucuya taşındı

--- saesra/settings/base.py
+++ saesra/settings/base.py
@@ -120,5 +120,5 @@ STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
-
+DEFAULT_FILE_STORAGE= 'deneme.storage_backend.MediaStorage'
 
