945baa947c65dd70bdb2c0d932c4021f33d1dfab
Akın Oslu <aknoslu3541@gmail.com>
2021-05-08 17:51:56 +0300

    aws ec2 sunucuya taşımadan önceki kodlar

--- saesra/settings/production.py
+++ saesra/settings/production.py
@@ -29,11 +29,11 @@ AUTH_PASSWORD_VALIDATORS = [
 DATABASES={
    'default':{
       'ENGINE':'django.db.backends.postgresql_psycopg2',
-      'NAME':env('DB_NAME'),
-      'USER':env('DB_USER'),
-      'PASSWORD':env('DB_PASSWORD'),
-      'HOST':'localhost',
-      'PORT':'5432',
+      'NAME': env('DB_NAME'),
+      'USER': env('DB_USER'),
+      'PASSWORD': env('DB_PASSWORD'),
+      'HOST': 'localhost',
+      'PORT': '5432',
    }
 }
 
